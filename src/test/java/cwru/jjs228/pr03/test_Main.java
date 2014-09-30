3d77a1f8ceb4f2761faa2c6ff9753b5eb3e394d4
Joseph Satterfield <jjs228@case.edu>
2014-09-30 11:23:33 -0400

    Initial commit with modified gitignore
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

---
+++ src/test/java/cwru/jjs228/pr03/test_Main.java
@@ -0,0 +1,25 @@
+package cwru.jjs228.pr03;
+
+
+import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import cwru.jjs228.pr03.Main;
+
+
+public class test_Main {
+
+    @Test
+    public void test_hello() {
+        assertEquals("hello", "hello");
+    }
+
+}
+
