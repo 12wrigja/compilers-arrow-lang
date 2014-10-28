536e8054e4be468e865a53d3669f10d57c1974d6
Joseph Satterfield <jjs228@case.edu>
2014-10-28 14:13:55 -0400

    Created typed node
    Signed-off-by: Joseph Satterfield <jjs228@case.edu>

---
+++ src/main/java/cwru/jjs228/pr03/TypedNode.java
@@ -0,0 +1,83 @@
+package cwru.jjs228.pr03;
+
+import sun.net.www.protocol.http.AuthCacheValue.Type;
+
+public class TypedNode<T> extends Node<T> {
+
+	
+	public Type type;
+
+	
+	public TypedNode(String label, Type type){
+		super(label);
+		this.type = type;
+	}
+	
+	public TypedNode(String label, T value, Type type){
+		super(value, label);
+		this.type = type;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof Node) {
+			return this.equals((TypedNode) o);
+		}
+		return false;
+	}
+	
+	public boolean equals(TypedNode n) {
+		if (!this.value.equals(n.value)) {
+			return false;
+		}
+		if (this.kids.size() != n.kids.size()) {
+			return false;
+		}
+		if (!this.type.equals(n.type)){
+			return false;
+		}
+		for (int i = 0; i < this.kids.size(); i++) {
+			if (!this.kids.get(i).equals(n.kids.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		if (kids.size() == 0) {
+			if(value != null && type != null){
+				return "("+label+","+value.toString()+type.toString()+")";
+			} else if(value != null){
+				return "("+label+","+value.toString()+")";
+			} else{
+				return "("+label+")";
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		for (Node kid : kids) {
+			sb.append(" ");
+			sb.append(kid);
+		}
+		String nodeString = "";
+		if(label != null && value != null && type != null){
+			nodeString = label+","+value + ":" + type.toString();
+		} else if(label != null && value != null){
+			nodeString = label +","+ value;
+		} else if(label != null && type != null){
+			nodeString = label + ":" + type.toString();
+		} else if (label != null){
+			nodeString = label;
+		} else if (value != null){
+			nodeString = value.toString();
+		} else if(type != null){
+			nodeString = type.toString();
+		} else{
+			nodeString = null;
+		}
+		return String.format("(%s%s)", null != value ? value : label, sb);
+	}
+	
+	
+}
