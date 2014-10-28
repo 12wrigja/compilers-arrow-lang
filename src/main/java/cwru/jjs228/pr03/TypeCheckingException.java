5b93b2e8cede00bdf4ff483603d778621d0cc796
jow5 <jow5@case.edu>
2014-10-28 15:52:44 -0400

    Update to fix implementation details with the native library functions, as well as start writing type checking rules
    Signed-off-by: jow5 <jow5@case.edu>

---
+++ src/main/java/cwru/jjs228/pr03/TypeCheckingException.java
@@ -0,0 +1,15 @@
+package cwru.jjs228.pr03;
+
+public class TypeCheckingException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TypeCheckingException(String message) {
+		super(message);
+	}
+
+	
+}
