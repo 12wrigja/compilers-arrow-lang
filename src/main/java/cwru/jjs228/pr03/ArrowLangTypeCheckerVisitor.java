10df0a37f2b7734f1eb4bb8b39d8da4859763326
jow5 <jow5@case.edu>
2014-10-28 14:47:25 -0400

    Update to add in the basics of the visitor pattern for type checking.
    Signed-off-by: jow5 <jow5@case.edu>

---
+++ src/main/java/cwru/jjs228/pr03/ArrowLangTypeCheckerVisitor.java
@@ -0,0 +1,112 @@
+package cwru.jjs228.pr03;
+
+public class ArrowLangTypeCheckerVisitor {
+	
+	public TypedNode visit(Node node){
+		
+	}
+	
+	public TypedNode visitIntConstant(Node node){
+		
+	}
+	
+	public TypedNode visitFloatConstant(Node node){
+		
+	}
+	
+	public TypedNode visitBoolConstant(Node node) {
+
+	}
+	
+	public TypedNode visitStringConstant(Node node) {
+
+	}
+	
+	public TypedNode visitSymbol(Node node) {
+
+	}
+	
+	public TypedNode visitStmts(Node node) {
+
+	}
+	
+	public TypedNode visitFuncDefStmt(Node node) {
+
+	}
+	
+	public TypedNode visitReturn(Node node) {
+
+	}
+	
+	public TypedNode visitDeclStmt(Node node) {
+
+	}
+	
+	public TypedNode visitShortDeclStmt(Node node) {
+
+	}
+	
+	public TypedNode visitAssignStmt(Node node) {
+
+	}
+	
+	public TypedNode visitIfElseStmt(Node node) {
+
+	}
+	
+	public TypedNode visitForStmt(Node node) {
+
+	}
+	
+	public TypedNode visitE(Node node) {
+
+	}
+	
+	public TypedNode visitBlock(Node node) {
+
+	}
+	
+	public TypedNode visitDeclExpr(Node node) {
+
+	}
+	
+	public TypedNode visitUpdateExpr(Node node) {
+
+	}
+	
+	public TypedNode visitBooleanExpr(Node node) {
+
+	}
+	
+	public TypedNode visitAnd(Node node) {
+
+	}
+	
+	public TypedNode visitOr(Node node) {
+
+	}
+	
+	public TypedNode visitNegateBoolean(Node node) {
+
+	}
+	
+	public TypedNode visitCmpOp(Node node) {
+
+	}
+	
+	public TypedNode visitCall(Node node) {
+
+	}
+	
+	public TypedNode visitCast(Node node) {
+
+	}
+	
+	public TypedNode visitArithOp(Node node) {
+
+	}
+	
+	public TypedNode visitNegateArith(Node node) {
+
+	}
+}
